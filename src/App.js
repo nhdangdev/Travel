29c215993b2f2217227a8858985119de269691a4
Nhựt Đăng <nhutdangtran98@gmail.com>
2023-06-14 11:21:25 +0700

    optimize and change Components  navbar to header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./scss/app.scss";
-import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Main from "./components/Main/Main";
@@ -9,11 +8,12 @@ import Footer from "./components/Footer/Footer";
 const App = () => {
   return (
     <>
-      <Navbar />
-      {/* 
-      <Home />
-      <Main />
-      <Footer /> */}
+      <Header />
+      <main>
+        <Home />
+        <Main />
+        <Footer />
+      </main>
     </>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
+import { IonIcon } from "@ionic/react";
+import { planetOutline, closeCircle } from "ionicons/icons";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-  return <div>Header</div>;
+  // Toggle Navbar
+  const [active, setActive] = useState("navbar");
+  const handleShowNav = () => {
+    setActive("navbar is-show");
+  };
+
+  // Remove Navbar
+  const removeNav = () => {
+    setActive("navbar");
+  };
+
+  return (
+    <div className="wrapper">
+      <header className="header">
+        <div className="container">
+          <div className="header-container">
+            <div className="header-logo">
+              <IonIcon icon={planetOutline} className="icon" />
+              Travel.
+            </div>
+            <div className={active}>
+              <ul className="navbar-list">
+                <li className="navbar-item">
+                  <a href="#" className="navbar-link">
+                    Home
+                  </a>
+                </li>
+                <li className="navbar-item">
+                  <a href="#" className="navbar-link">
+                    About
+                  </a>
+                </li>
+                <li className="navbar-item">
+                  <a href="#" className="navbar-link">
+                    Shop
+                  </a>
+                </li>
+                <li className="navbar-item">
+                  <a href="#" className="navbar-link">
+                    News
+                  </a>
+                </li>
+                {/* <div className="dropdown">
+                <li className="dropdown-select navbar-item">
+                  <a href="#" className="dropdown-selected navbar-link">
+                    Pages
+                  </a>
+                  <i className="fa fa-caret-down dropdown-caret"></i>
+                </li>
+                <div className="dropdown-list">
+                  <div className="dropdown__item">Dev</div>
+                  <div className="dropdown__item">Design</div>
+                  <div className="dropdown__item">Tester</div>
+                </div>
+              </div> */}
+                <li className="navbar-item">
+                  <a href="#" className="navbar-link">
+                    Contact
+                  </a>
+                </li>
+                <li className="navbar-item navbar-item--btn">
+                  <a href="#" className="btn btn--primary">
+                    Book Now
+                  </a>
+                </li>
+                <div className="navbar-close" onClick={removeNav}>
+                  <IonIcon icon={closeCircle} />
+                </div>
+              </ul>
+            </div>
+            <div className="navbar-btn">
+              <a href="#" className="btn btn--primary">
+                Book Now
+              </a>
+            </div>
+            <div className="navbar-toggle" onClick={handleShowNav}>
+              <FontAwesomeIcon icon={faBars} />
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Header;
